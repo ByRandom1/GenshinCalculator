99d7e33a1341c5c11b2d60716208775ff8bfeeb1
181860006 <future_important@163.com>
2023-03-17 21:50:34 +0800

    update config

---
+++ cmake-build-debug/RESULTS/hu_ye_wan_ban/run_optimized_config.ps1
@@ -0,0 +1,4 @@
+./run_optimized_config.bat hu_ye_wan_ban_1.txt > ./logs/hu_ye_wan_ban_1.txt
+./run_optimized_config.bat hu_ye_wan_ban_2.txt > ./logs/hu_ye_wan_ban_2.txt
+./run_optimized_config.bat hu_ye_wan_ban_3.txt > ./logs/hu_ye_wan_ban_3.txt
+./run_optimized_config.bat hu_ye_wan_ban_4.txt > ./logs/hu_ye_wan_ban_4.txt
---
+++ cmake-build-debug/RESULTS/hu_ye_wan_ban/run_substat_optimizer.ps1
@@ -0,0 +1,4 @@
+./run_substat_optimizer.bat hu_ye_wan_ban_1.txt
+./run_substat_optimizer.bat hu_ye_wan_ban_2.txt
+./run_substat_optimizer.bat hu_ye_wan_ban_3.txt
+./run_substat_optimizer.bat hu_ye_wan_ban_4.txt
